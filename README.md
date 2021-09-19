786739dfa63f5cb0ed3db22465d591c0d22e0dc9
lpc <1830910357@qq.com>
2021-09-19 12:21:30 +0800

    更新readme

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 # SUST_YiBan_Bot
+
 使用python自动化脚本来完成一些列操作
 部署到服务器上，可利用cron定时任务运行
 验证码采用了自己服务器的api来进行识别
+
+## 使用方法
+
+
+
+### 1.安装依赖
+
+```shell
+pip install -r requirements.txt
+```
+
+### 2.添加你要打卡的个人数据
+
+> 运行main.py
+
+```shell
+python main.py
+```
+
+> 运行结果如下
+
+<img src=".\mdimg\Snipaste_2021-09-19_12-05-34.png" alt="image-20210919120135069" style="zoom:50%;" />
+
+按照选项操作即可
+
+### 3.关于自动晨午检
+
+​	如果你是在windows上使用，可以参照这篇文章[使用Windows任务计划自动运行Python程序_若如初见-CSDN博客_任务计划程序运行python脚本](https://blog.csdn.net/Artificial_idiots/article/details/108570387)
+
+​	如果你是部署到自己的linux服务器上，直接使用crontab脚本自动执行
+
+
+
+每次打卡之后，相应的打卡信息会发送到你添加个人信息时的邮箱中
+
+
+
+## P.S. 之后会上线微信公众号版本的，直接在微信公众号里面进行操作
--- main.py
+++ main.py
@@ -1,1 +1,89 @@
-input("qinasd")
+from utils.sql import Sql
+import os
+
+
+def menu():
+    flag = 1
+    while flag:
+        print("----------------------------SUST晨午检小助手----------------------------")
+        print("1、管理用户")
+        print("2、查看服务运行状态")
+        print("3、启动自动晨午检服务")
+        print("4、添加晨午检反馈邮箱")
+        print("0、退出菜单")
+        aim = int(input())
+        if aim == 1:
+            manage_user()
+        elif aim == 2:
+            show_service_state()
+        elif aim == 3:
+            pass
+        elif aim == 4:
+            pass
+        elif aim == 0:
+            flag = 0
+            print("准备退出")
+        else:
+            print("请输入正确的选项")
+
+
+def start_service():
+    command = '10 7,12 * * * python ./auto_check_service.py'
+    p = os.system(command)
+    if p == 1:
+        print(p, "开启成功")
+
+
+def show_service_state():
+    print("正在运行")
+
+
+def manage_user():
+    flag = True
+    while flag:
+        print("----------------------------用户管理----------------------------")
+        print("1、查询用户")
+        print("2、添加用户")
+        print("3、删除用户")
+        print("4、更改地址")
+        print("5、返回上一级")
+        aim = int(input())
+        if aim == 1:
+            print(Sql.list_user(Sql()))
+        elif aim == 2:
+            add_user()
+        elif aim == 3:
+            delete_user()
+        elif aim == 4:
+            acc = input('请输入你要更改地址的账户')
+            while True:
+                address = input('请输入你要更改的新地址：')
+                if 'yes' == input('输入yes并回车确认更改：'):
+                    break
+            Sql.update_user(Sql(), address, acc)
+        elif aim == 5:
+            return
+
+
+def delete_user():
+    id = input('请输入删除的id')
+    if Sql.delete_user(Sql(), id):
+        print('删除成功')
+
+
+def add_user():
+    print("请依次输入要添加的账户、密码、信息上报地址、上报结果通知邮件")
+    account = input('账户:')
+    passwd = input('密码')
+    address = input('地址')
+    mail = input('通知邮件')
+    Sql.add_user(Sql(), account, passwd, address, mail)
+
+
+def main():
+    menu()
+    print("退出成功")
+
+
+if __name__ == '__main__':
+    main()
