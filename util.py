0df78cc8c772087aea4a8247b4a312e602569a16
lpcv5 <58157466+lpcv5@users.noreply.github.com>
2022-02-23 19:29:55 +0800

    Update util.py

--- util.py
+++ util.py
@@ -1,10 +1,9 @@
 import base64
-import os
 import re
 
 import requests
 import rsa
-
+import yaml
 
 key = "-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA6aTDM8BhCS8O0wlx2KzAAjffez4G4A" \
       "/QSnn1ZDuvLRbKBHm0vVBtBhD03QUnnHXvqigsOOwr4onUeNljegICXC9h5exLFidQVB58MBjItMA81YVlZKBY9zth1neHeRTWlFTCx" \
@@ -35,7 +34,7 @@ def get_token(acount, password):
         "v": "5.0.4",
         "apn": "wifi",
         "identify": "123123123123123",
-        "sig": "2asdasd",
+        "sig": "2",
         "token": "",
         "device": "xiaomi",
         "sversion": "30",
@@ -46,17 +45,44 @@ def get_token(acount, password):
     }
     r = requests.post("https://m.yiban.cn/api/v4/passport/login",
                       data=params, headers=headers)
+    print(r.json())
     return r.json()["data"]["access_token"]
 
 
-def getpath():
-    return os.path.split(os.path.realpath(__file__))[0]
+def check_token(token):
+    url = 'https://m.yiban.cn/api/v4/passport/autologin'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 10.0; xiaomi 10 Build/N2G47H; wv) AppleWebKit/537.36 (KHTML, '
+                      'like Gecko) Version/4.0 Chrome/95.0.4638.74 Mobile Safari/537.36 yiban_android/5.0.3',
+        'appversion': '5.0.3',
+        'logintoken': token,
+        'authorization': 'Bearer ' + token,
+    }
+    cookies = {
+        'https_waf_cookie': '531ce7b8-8574-4e6163b3cac842fe1d982190af5a2d644ea0'
+    }
+    response = requests.get(url, headers=headers, cookies=cookies)
+    if response.json()['response'] == 106:
+        return False
+    elif response.json()['response'] == 100:
+        return True
+    else:
+        return False
 
 
-def getssid(account, passwd):
+def write_account_token(data):
+    with open('./account.yaml', 'w', encoding='utf-8') as f:
+        yaml.dump(data=data, stream=f, allow_unicode=True)
+
+
+def read_account_data():
+    with open('./account.yaml', 'r', encoding="utf-8") as sys_fp:
+        sys_stream = sys_fp.read()
+        return yaml.load(sys_stream, Loader=yaml.SafeLoader)
+
+
+def get_ssid(token):
     try:
-        print(account, passwd)
-        token = get_token(account, passwd)
         appurl = "https://f.yiban.cn/iapp610661"
         headers = {
             'User-Agent': 'Mozilla/5.0 (Linux; Android 10.0; xiaomi 10 Build/N2G47H; wv) AppleWebKit/537.36 (KHTML, '
@@ -66,11 +92,10 @@ def getssid(account, passwd):
             'logintoken': token,
             'authorization': 'Bearer ' + token,
             'Cookie': 'loginToken=' + token + '; client=android'
-
         }
         response1 = requests.get(appurl, headers=headers, allow_redirects=False)
-        print('第一次重定向结果:', response1.headers["location"])
-        print(response1.cookies["https_waf_cookie"])
+        # print(response1.headers['location'])
+        # print(response1.headers['Set-Cookie'])
         headers = {
             'User-Agent': 'Mozilla/5.0 (Linux; Android 10.0; xiaomi 10 Build/N2G47H; wv) AppleWebKit/537.36 (KHTML, '
                           'like Gecko) Version/4.0 Chrome/95.0.4638.74 Mobile Safari/537.36 yiban_android/5.0.3',
@@ -78,22 +103,20 @@ def getssid(account, passwd):
             'signature': 'A3oHcLhQ8OCmXLuxOy4Y66TXyG/FrDhG+sCFRM1gXheN+LB0a9f3nVsnAgblO/0g7rPlHtfSob28RrKpwkdv9A',
             'logintoken': token,
             'authorization': 'Bearer ' + token,
-            'Cookie': 'loginToken=' + token + '; client=android;https_waf_cookie=' + response1.cookies["https_waf_cookie"]
+            'Cookie': 'loginToken=' + token + '; client=android;https_waf_cookie=' + response1.cookies[
+                "https_waf_cookie"]
         }
         response2 = requests.get(response1.headers['location'], headers=headers,
                                  allow_redirects=False)
-        print('第二次重定向结果:', response2.headers['location'])
-
+        # print(response2.headers['location'])
         response3 = requests.get(response2.headers['location'], allow_redirects=False)
         final_url = 'http://yiban.sust.edu.cn/v4/yibanapi/' + response3.headers['location']
-        print(final_url)
-
+        # print(final_url)
         response4 = requests.get(final_url, allow_redirects=False)
-        print(response4.headers)
         ssid = re.findall(r'PHPSESSID=(.+?);', response4.headers['Set-Cookie'])[0]
-        print(ssid)
+        if ssid is None:
+            print('ssid为空')
+        # print(ssid)
         return ssid
-    except:
-        pass
-
-
+    except ConnectionRefusedError as e:
+        print('获取ssid出现错误', e.args)
