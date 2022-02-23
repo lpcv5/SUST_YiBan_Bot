e66545e0170da140bed37362a13ceedf3b8e6d09
lpcv5 <58157466+lpcv5@users.noreply.github.com>
2022-02-23 19:29:04 +0800

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -1,22 +1,36 @@
-import json
 import re
+import threading
+import time
+
 import requests
+from readcaptcha.captch_crack_service import read
+from readcaptcha.config import Config
+from readcaptcha.event_loop import event_loop
+from readcaptcha.interface import InterfaceManager
 
 
-# 破解验证码
-def get_yzm(ssid):
-    url = 'http://yiban.sust.edu.cn/v4/public/index.php/admin/login/captcha.html'
-    cookies = {"PHPSESSID": ssid}
-    r = requests.post(url, cookies=cookies)
-    str1 = re.sub(r'\\', '', r.text)
-    # print(str1)
-    basecode = re.findall(r'"data:image/png;base64,(.+?)"', str1)[0]
-    # print(basecode)
-    uniqid = re.findall(r'"uniqid":"(.+?)"', str1)[0]
-    data = {
-        "image": basecode
-    }
-    coder = requests.post("http://216.127.186.79:19952/captcha/v1", data=data)
-    d = json.loads(coder.text)
-    code = d["message"]
-    return uniqid, code
+class CaptchaRead:
+    def __init__(self):
+        print('加载验证码识别服务')
+        self.interface_manager = InterfaceManager()
+        self.CONF_PATH = "readcaptcha/config.yaml"
+        self.MODEL_PATH = "readcaptcha/model"
+        self.GRAPH_PATH = "readcaptcha/graph"
+        self.SYSTEM_CONFIG = Config(conf_path=self.CONF_PATH, model_path=self.MODEL_PATH, graph_path=self.GRAPH_PATH)
+        # 开启识别验证码服务线程
+        captcha_service_thread = threading.Thread(
+            target=lambda: event_loop(self.SYSTEM_CONFIG, self.MODEL_PATH, self.interface_manager))
+        captcha_service_thread.daemon = True
+        captcha_service_thread.start()
+        time.sleep(1)
+        print('验证码识别服务加载成功')
+
+    def get_yzm(self, ssid):
+        url = 'http://yiban.sust.edu.cn/v4/public/index.php/admin/login/captcha.html'
+        cookies = {"PHPSESSID": ssid}
+        r = requests.post(url, cookies=cookies)
+        str = re.sub(r'\\', '', r.text)
+        basecode = re.findall(r'"data:image/png;base64,(.+?)"', str)[0]
+        uniqid = re.findall(r'"uniqid":"(.+?)"', str)[0]
+        code = read(basecode, self.SYSTEM_CONFIG, self.interface_manager)
+        return uniqid, code
