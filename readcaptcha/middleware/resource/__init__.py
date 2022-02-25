db756114ffcd3ff06f450758115a7eea94674fd3
lpcv5 <58157466+lpcv5@users.noreply.github.com>
2022-02-25 21:14:18 +0800

    Delete readcaptcha directory

--- readcaptcha/middleware/resource/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-# Author: kerlomz <kerlomz@gmail.com>
-from middleware.resource.color_filter import model_onnx
-
-color_model = model_onnx
-if __name__ == '__main__':
-    pass
